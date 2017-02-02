33e590a13ab36918ae379ab94b994daa4ebdaaf7
Johnathan Hinebrook <johnisadept@live.com>
2017-02-02 11:22:56 -0500

    Create googlehaxer.php

---
+++ googlehaxer.php
@@ -0,0 +1,36 @@
+<?php
+//https://www.google.com/search?as_q=nintendo+wii
+
+$googlestring = "https://www.google.com/search?as_q="
+$intitle = "intitle:"
+$fileExt = ""
+
+?>
+
+<html>
+<style>
+
+</style>
+
+<table align="center">
+<form>
+
+<tr "center"><td align="center"><img src="http://img06.deviantart.net/183d/i/2012/311/5/8/vocaloid_doodle_by_kory_chan-d5kaex6.jpg" height="400" width="400"></td></tr>
+<tr "center"><td align="center">Search <input type="text" name="search"></td></tr>
+
+<tr "center"><td align="center">|<input type="radio" name="intitle"> intitle| </td></tr>
+<tr "center"><td align="center">
+|
+<input type="radio" name="mp3">  mp3 |
+<input type="radio" name="mp4"> mp4 |
+<input type="radio" name="avi"> avi |
+<input type="radio" name="mkv"> mkv |
+</tr></td><tr align="center"><td align="center">
+<br>
+<input type="submit"></tr></td>
+</form>
+<tr "center"><td align="center"><br><br><br>
+notes: <a href="http://www.our-picks.com/archives/2007/01/30/google-search-urls-revealed-or-how-to-create-your-own-search-url/">our-picks.com</a>
+</td></tr>
+
+</html
